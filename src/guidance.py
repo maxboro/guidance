6dc95e5d36af283e44b51fe9e9188bf940fff4aa
Max B. <maksym_borovyk@ukr.net>
2025-08-14 11:49:26 +0300

    Add velocity (closing and interceptor) component - without it breaks in some cases

--- src/guidance.py
+++ src/guidance.py
@@ -23,22 +23,30 @@ class PropNav(Guidance):
     def __init__(self, interceptor: Unit, target: Unit):
         super().__init__(interceptor, target)
         self._prev_los_vector_angle = None
-        self.gain = 5
+        self._prev_interceptor_coords = None
+        self._prev_distance = None
+        self.gain = 4
         self.dt = 1
         self._gamma_deg = None  # internal missile heading
 
     def get_angle(self) -> float:
         los_vector = self.target.coords - self.interceptor.coords
         los_vector_angle = math.degrees(np.arctan2(los_vector[1], los_vector[0]))
-
+        distance = np.linalg.norm(los_vector)
         if self._prev_los_vector_angle is None and self._gamma_deg is None:
             los_rate_deg = 0
             self._gamma_deg = los_vector_angle
+            gamma_rate_deg = velocity_closing = velocity_interseptor = 0 # for printing
         else:
+            velocity_closing =-(distance - self._prev_distance)
+            velocity_interseptor = np.linalg.norm(self.interceptor.coords - self._prev_interceptor_coords)
+            vc_2_vi = velocity_closing / velocity_interseptor if velocity_interseptor != 0 else 1
             los_rate_deg = wrap_deg(los_vector_angle - self._prev_los_vector_angle) / self.dt
-            gamma_rate_deg = los_rate_deg * self.gain
+            gamma_rate_deg = los_rate_deg * self.gain * vc_2_vi
             self._gamma_deg = wrap_deg(self._gamma_deg + gamma_rate_deg * self.dt)
         self._prev_los_vector_angle = los_vector_angle
+        self._prev_interceptor_coords = self.interceptor.coords.copy()
+        self._prev_distance = distance
         return self._gamma_deg
 
 def get_guidance(guidance_mode: str) -> Guidance:
