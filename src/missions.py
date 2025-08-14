b1088fa902062c0114d441d755f2574aba2e2bb1
Max B. <maksym_borovyk@ukr.net>
2025-08-14 12:30:25 +0300

    Fix: mission inheritance

--- src/missions.py
+++ src/missions.py
@@ -6,11 +6,11 @@ class TargetMission(ABC):
     def get_angle(self, n_step: int, sim_n_steps: int) -> float:
         pass
 
-class BallisticAttackFromRight:
+class BallisticAttackFromRight(TargetMission):
     def get_angle(self, n_step: int, sim_n_steps: int) -> float:
         return 150 + ((n_step + 1) / sim_n_steps) * 80
 
-class Pursuit:
+class Pursuit(TargetMission):
     def get_angle(self, n_step: int, sim_n_steps: int) -> float:
         return 0 + ((n_step + 1) / sim_n_steps) * 80
 
